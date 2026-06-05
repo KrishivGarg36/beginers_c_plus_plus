d7bea3c13f2150dea0b9821d584774f62422e55f
KrishivGarg36 <krishivgarg935@gmail.com>
2026-06-05 15:37:41 +0530

    Add files via upload

---
+++ tut8.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+    //int a = 34;
+    //cout<<"The value of a was: "<<a;
+    //a = 45;
+    //cout<<"The value of a is: "<<a;
+    //constants in c++
+    //const int x = 3.14;
+    //cout<<"The value of x is: "<<x<<endl
+   // x = 45;// You will get an error because x is a constant
+   //cout<<"The value of a is: "<<x<<endl;
+   // Manipulator in c++
+  // int a =3, b =78, c =1233,
+   //cout<<"The value a is: "<<setw(4)<<a<<end1;
+   //cout<<"The value of b is: "<<setw(4)<<b<<endl;
+   //cout<<"The value of c is: "<<setw(4)<<c<<endl;
+
+   //cout<<"The value a without setw is: "<<a<<end1;
+   //cout<<"The value of b without setw is: "<<b<<end1;
+   //cout<<"The value of c without setw is "<<c<<end1;
+
+
+   //Operator Precedence
+   int a = 3, b = 4
+   //int c = (a*5)+b;
+   int c = ((((a*5)+b)-45)+87)
+   cout<<c;
+   
+   return 0;
+}
