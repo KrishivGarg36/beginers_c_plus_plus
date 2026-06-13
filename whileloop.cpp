38d6e1a923db22d379bb22db95a23b8d6f92d868
KrishivGarg36 <krishivgarg935@gmail.com>
2026-06-13 20:46:27 +0530

    Add files via upload

---
+++ whileloop.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+/*int main(){
+
+    int n;
+    cin>>n;
+
+    int i = 1;
+    while (i<=n) {
+
+        cout<<i<<" ";
+        i = i+1;
+    }
+}*/
+
+/*int main(){
+
+    int n;
+    cin >> n;
+
+    int i = 1;
+    int sum = 0;
+
+    while(i<=n) {
+        sum = sum + i;
+        i = i + 1;
+    }
+
+    cout<<"value of sum is"<<sum<<endl;
+}*/
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    int i = 2;
+
+    while(i<n) {
+        //divide hogya,not prime
+        if(n%i==0) {
+            cout<<"Not Prime"<< i << endl;
+
+        }
+        else{
+            cout<<"Prime for"<< i <<endl;
+
+        }
+        i = i + 1;
+    }
+}
