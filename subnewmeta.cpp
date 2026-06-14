64719c9471a3877a628728a5cc319e2719e8adce
KrishivGarg36 <krishivgarg935@gmail.com>
2026-06-14 12:52:56 +0530

    Add files via upload

---
+++ subnewmeta.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    /*int a ;
+    cout<<"Enter the value of a "<<endl;
+    cin>>a;
+
+    if(a>0) {
+        cout<<"A is positive";
+    }
+    else{
+        if(a<0) {
+            cout<<"A is negative";
+        }
+        else{
+            cout<<"A is 0";
+        }    
+
+        
+    }*/
+   int a = 2;
+   int b = a+1;
+   
+   if((a=3)==b) {
+    cout<<a;
+   }
+   else{
+    cout<<a+1;
+   }
+
+}
