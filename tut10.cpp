607a013630e0f68a662e1889f5fa2f4a5906780b
KrishivGarg36 <krishivgarg935@gmail.com>
2026-06-14 12:49:25 +0530

    Add files via upload

---
+++ tut10.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+int main(){
+    /*Loops in c++:
+    There are three types of loops in c++
+    1) For loop
+    2) While loop
+    3) do-while loop
+    */
+
+    /*For loop in c++*/
+    //int i=1;
+    //cout<<i;
+    //i++;
+
+    //Syntax for for loop
+    //for(initialisation; condition; updation)
+    //{
+    //   loop body(c++ code);
+    //}
+
+    //for (int i = 0; i < 40; i++)
+    //{
+       // std::ostream std::cout
+       // cout<<i<<endl;
+    //}
+    //Example of infinite for loop
+   // for (int i =1; 34 <= 40; i++)
+    //{
+        /* code */
+        //cout<<i<<endl;
+    //}
+
+        /* While loop in c++ */
+        // Syntax:
+        // while(condition)
+        //{
+        //    C++ statements;
+        //}
+        // Printing 1 to 40 using while loop
+        //int i=1;
+        //while(i<=40){
+           // cout<<i<<endl;
+            //i++;
+        //}
+        //Example of infinite while loop
+        //int i=1;
+        //while(true){
+            //cout<<i<<endl;
+            //i++;
+        //}
+
+        /*Do while loop in c++*/
+        // Syntax:
+        //do
+        // {
+        //      c++ statements;
+        // }while(condition)
+
+
+    // Printing 1 to 40 using while loop
+    int i=1;
+    do{
+        cout<<i<<endl;
+        i++;
+    }while(false);
+
+
+
+    return 0;
+
+}
